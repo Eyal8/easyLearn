39150549fbfd977690bd00dd3d4deaac5fe06780
Eyal Arviv <eyal8arviv@gmail.com>
2018-03-22 11:17:37 +0200

    init commit

---
+++ app/src/main/java/com/example/easylearn/GroupScreen.java
@@ -0,0 +1,13 @@
+package com.example.easylearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_screen);
+    }
+}
